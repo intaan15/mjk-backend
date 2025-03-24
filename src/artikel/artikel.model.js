26ecba247066d314485cd381293510ca0adcec85
adit-wr <ganding0002nd@gmail.com>
2025-03-24 20:03:01 +0700

    add model artikel

---
+++ src/artikel/artikel.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const artikelSchema = new mongoose.Schema({
+    nama_artikel: {
+        type: String,
+        required: true,
+    },
+    tgl_terbit_artikel: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    detail_artikel: {
+        type: String,
+        required: true,
+    },
+    gambar_artikel: {
+        type: String,
+        required: true,
+    },
+    kategori_artikel:{
+        type: String,
+        enum: ["Kesehatan", "Obat"],
+        required: true,
+    },
+})
+
+module.exports = mongoose.model("artikel", artikelSchema, "artikel");
