506f6fa2f05eb4508fef1a893766528de402774a
Intan Lailatul Ilmiana <intaan15@github.com>
2025-03-27 21:53:37 +0700

    add update delete rating

--- src/rating/rating.controller.js
+++ src/rating/rating.controller.js
@@ -27,4 +27,34 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.patch("/update/:id", async (req, res) => {
+    try {
+        const { rating: ratingValue } = req.body;
+
+        if (ratingValue < 0 || ratingValue > 5) {
+            return res.status(400).json({ message: "rating harus berada di antara 0 hingga 5." });
+        }
+
+        const updatedRating = await rating.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedRating) {
+            return res.status(404).json({ message: "rating tidak ditemukan" });
+        }
+        res.status(200).json(updatedRating);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.delete("/delete/:id", async (req, res) => {
+    try {
+        const deletedRating = await rating.findByIdAndDelete(req.params.id);
+        if (!deletedRating) {
+            return res.status(404).json({ message: "rating tidak ditemukan" });
+        }
+        res.status(200).json({ message: "rating berhasil dihapus" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 module.exports = router;
