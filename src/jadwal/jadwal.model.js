1380958f84c826282c9c7ce73b3d2735714c6b34
Intan Lailatul Ilmiana <intaan15@github.com>
2025-04-08 10:26:50 +0700

    add model jadwal

---
+++ src/jadwal/jadwal.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const jadwalSchema = new mongoose.Schema({
+    verifikasi_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "verifikasi", 
+        required: true,
+    },
+    dokter_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "dokter", 
+        required: true,
+    },
+    tgl_konsul: {
+        type: Date,
+        required: true,
+    },
+    jumlah_konsul: {
+        type: Number,
+        required: true,
+    },
+    status_konsul: {
+        type: String,
+        enum: ["menunggu", "ditolak", "diterima", "berlangsung", "selesai"],
+        default: "menunggu",
+        required: true,
+    },
+});
+
+module.exports = mongoose.model("jadwal", jadwalSchema, "jadwal");
