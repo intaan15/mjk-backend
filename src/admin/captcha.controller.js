48984005c27dd42a923c177094d5f35cdb353a3c
rayhanizzudd <rayhannavis11@gmail.com>
2025-04-14 09:26:19 +0700

    add : captcha superadmin

---
+++ src/admin/captcha.controller.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+const crypto = require("crypto");
+
+const captchaStore = {};
+
+function generateCaptchaText() {
+  return Math.random().toString(36).substring(2, 8);
+}
+
+// Endpoint untuk generate captcha
+router.get("/captcha", (req, res) => {
+  const captchaText = generateCaptchaText();
+  const captchaId = crypto.randomUUID();
+
+  // Simpan captcha ke store
+  captchaStore[captchaId] = captchaText;
+
+  setTimeout(() => {
+    delete captchaStore[captchaId];
+  }, 15000); 
+
+  res.json({
+    captcha: captchaText, 
+    captchaId, 
+    expiresIn: 10, 
+  });
+});
+
+// Endpoint untuk validasi captcha
+router.post("/validate", (req, res) => {
+  const { captchaId, userInput } = req.body;
+
+  console.log("Received captchaId:", captchaId);
+  console.log("captchaStore:", captchaStore);
+  console.log("User input:", userInput);
+
+  const storedCaptcha = captchaStore[captchaId];
+
+  if (!storedCaptcha) {
+    return res.status(400).json({
+      success: false,
+      message: "Captcha tidak valid atau sudah kedaluwarsa",
+    });
+  }
+
+  // Cek kecocokan captcha
+  if (
+    typeof userInput === "string" &&
+    storedCaptcha.toLowerCase() === userInput.toLowerCase()
+  ) {
+    delete captchaStore[captchaId];
+    return res.json({ success: true });
+  }
+
+  return res.json({
+    success: false,
+    message: "Captcha salah",
+  });
+});
+
+module.exports = router;
