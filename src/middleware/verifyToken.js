142ff034c3eb65b8358b4725bc31daf31662c617
adit-wr <ganding0002nd@gmail.com>
2025-04-22 08:43:33 +0700

    add middleware verify token

---
+++ src/middleware/verifyToken.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+function verifyToken(req, res, next) {
+  const token = req.header("Authorization");
+  if (!token) {
+    return res.status(401).json({ message: "Akses ditolak, token tidak ditemukan" });
+  }
+
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(400).json({ message: "Token tidak valid" });
+  }
+}
+
+module.exports = verifyToken;
