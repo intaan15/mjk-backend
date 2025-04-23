c3564c0af358b4db328b91c58d7239f1eb99e46e
rayhanizzudd <rayhannavis11@gmail.com>
2025-04-23 13:48:35 +0700

    update : rate limit in login dokter

--- src/middleware/ratelimiter.js
+++ src/middleware/ratelimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 menit
-  max: 10, // max 10 percobaan
+  max: 2, // max 10 percobaan
   message: "Terlalu banyak percobaan login. Coba lagi nanti.",
 });
 
