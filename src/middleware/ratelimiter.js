e66b9489717d05cf14d0f756b7f47ea125121557
rayhanizzudd <rayhannavis11@gmail.com>
2025-05-14 10:13:31 +0700

    update : limiter middleware

--- src/middleware/ratelimiter.js
+++ src/middleware/ratelimiter.js
@@ -1,9 +1,10 @@
-// const rateLimit = require("express-rate-limit");
+const rateLimit = require("express-rate-limit");
 
-// const loginLimiter = rateLimit({
-//   windowMs: 1 * 60 * 1000, // 15 menit
-//   max: 3, // max 10 percobaan
-//   message: "Terlalu banyak percobaan login. Coba lagi nanti.",
-// });
+const loginLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 15 menit
+  max: 3, // max 10 percobaan
+  statusCode: 429,
+  message: "Terlalu banyak percobaan login. Coba lagi nanti.",
+});
 
-// module.exports = loginLimiter;
+module.exports = loginLimiter;
