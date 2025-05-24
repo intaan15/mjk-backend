9dda843ec6405f3d588c5b3d612a72579d896814
rayhanizzudd <rayhannavis11@gmail.com>
2025-05-24 12:37:51 +0700

    update : model chat

--- src/socket/chat.model.js
+++ src/socket/chat.model.js
@@ -4,9 +4,12 @@ const mongoose = require("mongoose");
 const chatSchema = new mongoose.Schema({
   dari: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   ke: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  isi: { type: String, required: true },
+  text: { type: String },
+  image: { type: String },
+  type: { type: String, enum: ["text", "image"], required: true },
   role: { type: String, enum: ["dokter", "masyarakat"], required: true },
   waktu: { type: Date, default: Date.now },
 });
+  
 
 module.exports = mongoose.model("Chat", chatSchema);
