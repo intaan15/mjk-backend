871bb495b5485ca1e996b0be0defc8dd88ee6f77
rayhanizzudd <rayhannavis11@gmail.com>
2025-06-02 11:12:45 +0700

    try : image socket

--- src/socket/chat.model.js
+++ src/socket/chat.model.js
@@ -2,18 +2,10 @@
 const mongoose = require("mongoose");
 
 const chatSchema = new mongoose.Schema({
-  senderId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  receiverId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
+  senderId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  receiverId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   image: { type: String, default: null }, // penting!
-  type: { type: String, default: "text" },
+  text: { type: String, default: "text" },
   type: { type: String, enum: ["text", "image"], required: true },
   role: { type: String, enum: ["dokter", "masyarakat"], required: true },
   waktu: { type: Date, default: Date.now },
