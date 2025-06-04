e077ee29720b6f5eb3e99fc0ba914cc18b475d1e
rayhanizzudd <rayhannavis11@gmail.com>
2025-06-04 13:17:40 +0700

    update : pake disable chaT

--- src/socket/socket.controller.js
+++ src/socket/socket.controller.js
@@ -62,15 +62,14 @@ const createSocketServer = (server) => {
           return;
         }
 
-        const chatList = await ChatList.findOne({
-          "participants.user": { $all: [msg.senderId, msg.receiverId] },
-          jadwal: msg.jadwalId,
-        }).populate("jadwal");
-        
-
+        // Cari chatlist terkait dan populate jadwal
         // const chatList = await ChatList.findOne({
         //   "participants.user": { $all: [msg.senderId, msg.receiverId] },
         // }).populate("jadwal");
+
+        const chatList = await ChatList.findOne({
+          "participants.user": { $all: [msg.senderId, msg.receiverId] },
+        }).populate("jadwal");
         
 
         if (!chatList) {
@@ -130,6 +129,79 @@ const createSocketServer = (server) => {
         console.error("Error saat simpan pesan:", err.message);
       }
     });
+
+    // BESOK LAGI INI 
+    // socket.on("chat message", async (msg) => {
+    //   try {
+    //     if (!msg.senderId || !msg.receiverId) {
+    //       console.warn("Pesan tidak lengkap:", msg);
+    //       return;
+    //     }
+
+    //     const chatList = await ChatList.findOne({
+    //       "participants.user": { $all: [msg.senderId, msg.receiverId] },
+    //       jadwal: msg.jadwalId,
+    //     }).populate("jadwal");
+
+    //     // const chatList = await ChatList.findOne({
+    //     //   "participants.user": { $all: [msg.senderId, msg.receiverId] },
+    //     // }).populate("jadwal");
+
+    //     if (!chatList) {
+    //       return socket.emit("errorMessage", {
+    //         message: "Sesi chat tidak ditemukan.",
+    //       });
+    //     }
+
+    //     // Validasi apakah sesi sudah selesai
+    //     const jadwal = chatList.jadwal;
+    //     const [hour, minute] = jadwal.jam_konsul.split(":").map(Number);
+    //     const startTime = new Date(jadwal.tgl_konsul);
+    //     startTime.setHours(hour);
+    //     startTime.setMinutes(minute);
+    //     startTime.setSeconds(0);
+
+    //     const endTime = new Date(startTime.getTime() + 3 * 60 * 1000); // 3 menit
+
+    //     const now = new Date();
+    //     if (jadwal.status_konsul === "selesai") {
+    //       return socket.emit("errorMessage", {
+    //         message:
+    //           "⛔ Konsultasi telah selesai. Anda tidak dapat mengirim pesan.",
+    //       });
+    //     }
+
+    //     // Lanjut jika valid
+    //     const newMsg = new Chat({
+    //       text: msg.text || "",
+    //       sender: msg.sender || "User",
+    //       senderId: msg.senderId,
+    //       receiverId: msg.receiverId,
+    //       image: msg.image || null,
+    //       type: msg.type || "text",
+    //       role: msg.role || "unknown",
+    //       waktu: msg.waktu || now,
+    //     });
+
+    //     const savedMsg = await newMsg.save();
+
+    //     // Update chatlist
+    //     if (chatList) {
+    //       chatList.lastMessage =
+    //         msg.text || (msg.type === "image" ? "📷 Gambar" : "Pesan baru");
+    //       chatList.lastMessageDate = new Date();
+    //       const currentUnread = chatList.unreadCount.get(msg.receiverId) || 0;
+    //       chatList.unreadCount.set(msg.receiverId, currentUnread + 1);
+    //       await chatList.save();
+    //     }
+
+    //     // Emit ke dua user
+    //     io.to(savedMsg.receiverId.toString()).emit("chat message", savedMsg);
+    //     io.to(savedMsg.senderId.toString()).emit("chat message", savedMsg);
+    //   } catch (err) {
+    //     console.error("Error saat simpan pesan:", err.message);
+    //   }
+    // });
         
 
     socket.on("disconnect", () => {
