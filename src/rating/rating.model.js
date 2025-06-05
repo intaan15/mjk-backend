3a61e540a6a6e7dae80242d758ffc95145689139
Intan Lailatul Ilmiana <intaanlailatul@gmail.com>
2025-06-05 14:58:42 +0700

    memperbaiki model rating

--- src/rating/rating.model.js
+++ src/rating/rating.model.js
@@ -1,31 +1,28 @@
 const mongoose = require("mongoose");
 
 const ratingSchema = new mongoose.Schema({
-    masyarakat_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "masyarakat",
-        required: true,
-    },
-    dokter_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "dokter",
-        required: true,
-    },
-    nama_masyarakat: {
-        type: String,
-        required: true,
-    },
-    nama_dokter: {
-        type: String,
-        required: true,
-    },
-    rating: {
-        type: Number,
-        required: true,
-        min: 0,
-        max: 5,
-    },
-}, { timestamps: true }
-);
+   jadwal: {
+       type: mongoose.Schema.Types.ObjectId,
+       ref: "jadwal",
+       required: true,
+   },
+   masyarakat_id: {
+       type: mongoose.Schema.Types.ObjectId,
+       ref: "masyarakat",
+       required: true,
+   },
+   dokter_id: {
+       type: mongoose.Schema.Types.ObjectId,
+       ref: "dokter",
+       required: true,
+   },
+   rating: {
+       type: Number,
+       required: true,
+       min: 0,
+       max: 5,
+   },
+}, { timestamps: true });
 
+ratingSchema.index({ jadwal: 1, masyarakat_id: 1 }, { unique: true });
 module.exports = mongoose.model("rating", ratingSchema, "rating");
