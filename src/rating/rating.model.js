ae8faaffff72d2629ba231db5f5166222bb54af4
Intan Lailatul Ilmiana <intaanlailatul@gmail.com>
2025-06-12 11:12:16 +0700

    update model rating

--- src/rating/rating.model.js
+++ src/rating/rating.model.js
@@ -1,28 +1,43 @@
 const mongoose = require("mongoose");
 
 const ratingSchema = new mongoose.Schema({
-   jadwal: {
-       type: mongoose.Schema.Types.ObjectId,
-       ref: "jadwal",
-       required: true,
-   },
-   masyarakat_id: {
-       type: mongoose.Schema.Types.ObjectId,
-       ref: "masyarakat",
-       required: true,
-   },
-   dokter_id: {
-       type: mongoose.Schema.Types.ObjectId,
-       ref: "dokter",
-       required: true,
-   },
-   rating: {
-       type: Number,
-       required: true,
-       min: 0,
-       max: 5,
-   },
+  jadwal: {
+    type: mongoose.Schema.Types.ObjectId, 
+    required: true,
+  },
+  masyarakat_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Masyarakat", 
+    required: true,
+  },
+  dokter_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Dokter", 
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 5,
+  },
 }, { timestamps: true });
 
 ratingSchema.index({ jadwal: 1, masyarakat_id: 1 }, { unique: true });
-module.exports = mongoose.model("rating", ratingSchema, "rating");
+
+ratingSchema.pre("save", async function (next) {
+  try {
+    const dokter = await mongoose.model("Dokter").findOne({
+      _id: this.dokter_id,
+      "jadwal._id": this.jadwal,
+    });
+    if (!dokter) {
+      throw new Error("Jadwal atau dokter tidak valid");
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = mongoose.model("Rating", ratingSchema, "rating");
